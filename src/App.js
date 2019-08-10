3bffada6c052c2989029674ea331a7b7911ee523
Lindsay Chapin <lnchapin@gmail.com>
2019-08-09 20:16:28 -0600

    adding icons building out day card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
+import moment from 'moment';
+import { Container, Row } from 'reactstrap';
 import DayCard from './components/DayCard';
 import DayDetail from './components/DayDetail';
 import Header from './components/Header';
-import { Container, Row } from 'reactstrap';
 import data from './components/data/sample.json';
 
-console.log(data);
 
 class App extends Component{
   state = {
     days:data.data
+  }
+
+  componentDidMount(){
+
   }
 
   render(){
@@ -20,7 +24,17 @@ class App extends Component{
         </Row>
         <Row>
           {this.state.days.map(day =>(
-            <DayCard key={day.ts}/>
+            <DayCard
+            key={day.ts}
+            current={day.temp}
+            high={day.max_temp}
+            low={day.min_temp}
+            pricp={day.pop}
+            day={moment(day.datetime, 'YYYY-MM-DD').format('dddd')}
+            dewpt={day.dewpt}
+            icon={day.weather.icon}
+            alt={day.weather.description}
+            />
           ))}
         </Row>
         <Row>
--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Col, Card, CardHeader, CardBody } from 'reactstrap';
+import styled from 'styled-components'
 
-const DayCard = () =>{
+const CardWrapper = styled.div`
+  color: turquoise;
+  .card{
+    background: papayawhip;
+    h3{
+      font-weight: 900;
+    }
+    img{
+      width: 75px;
+    }
+  }
+`
+
+
+const DayCard = props =>{
+  console.log(props);
   return(
-    <h3>Day Card</h3>
+    <Col>
+      <CardWrapper>
+        <Card>
+          <CardHeader>{props.day}</CardHeader>
+          <CardBody>
+            <h3>{props.current.toFixed(1)}°</h3>
+            <img src={`${process.env.PUBLIC_URL}/icons/${props.icon}.png`} alt={props.alt}/>
+            <p>{props.low}/{props.high}</p>
+            <p>Chance of precip: {props.pricp}%</p>
+          </CardBody>
+        </Card>
+      </CardWrapper>
+    </Col>
   )
 }
 
